0a41c6db7d5365d0a270efb3511bbd94360cac17
shashavali <starshashavali369@gmail.com>
2022-08-12 22:20:29 +0530

    ARRAY TO STRING

---
+++ src/ArrayToString.java
@@ -0,0 +1,10 @@
+public class ArrayToString {
+    public static void main(String[] args) {
+        String[] s = {"convert", "By", "Array"};
+        String s1 = convertArraytoString(s);
+        System.out.println("Convert Array to String:" + s1);
+    }
+    public  static  String  convertArraytoString(String[] s){
+        return  convertArraytoString(s);
+    }
+}
